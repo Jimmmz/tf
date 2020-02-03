da98de603f7bef8c566784ae43dc3eaf8b851202
Jimmmz <james@jstewart.org.uk>
2020-02-03 10:34:08 +0000

    added SG etc

--- main.tf
+++ main.tf
@@ -6,12 +6,33 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
+# create an instance using an AMI
+resource "aws_instance" "js-instance" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  
+
+  # create an index.html file when the server starts up. nohup is a builtin Ubuntu http service  
+  user_data = <<-EOF
+          #!/bin/bash
+          echo "Hello James" > index.html
+          nohup busybox httpd -f -p 8080 &
+          EOF
+#  always set owner for any Hashi instance
   tags = {
     Owner = "jstewart@hashicorp.com"
     Name = "JStewart"
   } 
 }
+  
+# create a security group. AWS by default does not allow traffic on or out of an instance
+resource "aws_security_group" "js-sg" {
+  name = "js-sec-group"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+  

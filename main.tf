ecd3bd5593dc621a770db132f8db6d69db491be8
Jimmmz <james@jstewart.org.uk>
2020-02-07 12:44:19 +0000

    final working terraform, end of chapter 2

--- main.tf
+++ main.tf
@@ -8,14 +8,14 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-# define aws deafult VPC
-data "aws_vpc" "default" {
+# define aws default VPC
+data "aws_vpc" "js-default" {
   default = true
 }
 
 # pull subnet ID's from default VPC
 data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id  
+  vpc_id = data.aws_vpc.js-default.id  
 }
 
 # create an instance using an AMI
@@ -105,7 +105,13 @@ resource "aws_autoscaling_group" "js-asg" {
   launch_configuration = aws_launch_configuration.js-lc.name
 # Define subnet ID's - page 64  
   vpc_zone_identifier = data.aws_subnet_ids.default.ids
+  desired_capacity = 3
 
+
+# Define target group that the ASG will use
+  target_group_arns = [aws_lb_target_group.js_alb_tg.arn]
+  health_check_type = "ELB"
+  
   max_size = "10"
   min_size = "2"
 
@@ -114,7 +120,6 @@ resource "aws_autoscaling_group" "js-asg" {
     value = "JStewart-ASG"
     propagate_at_launch = true
   }
-  
   tag {
     key = "Owner"
     value = "jstewart@hashicorp.com"
@@ -122,5 +127,99 @@ resource "aws_autoscaling_group" "js-asg" {
   }
 }
 
+resource "aws_lb" "js_alb" {
+  name_prefix = "js-"
+  load_balancer_type = "application"
+#  security_groups = "Optional"
+  subnets = data.aws_subnet_ids.default.ids
+  security_groups = [aws_security_group.js_alb_sg.id]
+  
+  tags = {
+    Owner = "jstewart@hashicorp.com"
+    Name = "JStewart"
+  } 
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.js_alb.arn
+  port = "80"
+  protocol = "HTTP"
+
+#by default return a 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Dude! Where's my page?"
+      status_code = "404"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "listen_rule" {
+  listener_arn = aws_lb_listener.http_listener.arn
+  priority = "100"
+  
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.js_alb_tg.arn
+  }
+# https://www.terraform.io/docs/providers/aws/r/lb_listener_rule.html#path_pattern  
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
+
+resource "aws_security_group" "js_alb_sg" {
+  name = "js_alb_security_group"
+  description = "sg description"
+  
+# Allow incoming HTTP requests
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+#Allow all outbound requests
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "js_alb_tg" {
+  name = "js-target-group"
+  # name_prefix = "js-"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.js-default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    
+  }
+}  
+
+# set an ouptput variable to get the public IP adress of the server
+output "alb_dns_name" {
+  value = aws_lb.js_alb.dns_name
+  description = "The Load Balancers DNS name"
+}
 
 
+  # tags {
+  #   Owner = "jstewart@hashicorp.com"
+  # }
